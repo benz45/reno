12cdd3221d5a0fd0bd4b6af886140ff7846f83e6
Pao Veerapan <veerapan.boo@turbo.co.th>
2023-08-15 11:11:03 +0700

    init commit

---
+++ src/test/java/com/reno/reno/RenoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reno.reno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
