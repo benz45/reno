8281ff9c8f3fa470b7f5a51e25a33b8e02479837
Veerapan Boonbuth <benz45.th@gmail.com>
2023-09-26 10:19:57 +0700

    feature: add service get env

--- Dockerfile
+++ Dockerfile
@@ -9,13 +9,23 @@ COPY target/reno-0.0.1-SNAPSHOT.jar .
 EXPOSE 8080
 ENTRYPOINT ["java","-jar","reno-0.0.1-SNAPSHOT.jar"]
 
+# install python
+RUN apt-get update
+RUN apt-get -y install python3-pip
+
+# install aws
+RUN pip3 install awscli
+
+# install jq
+RUN apt-get update && apt-get install -y jq
 
 RUN mkdir -p BOOT-INF/classes/
 ADD src/main/resources/application-template.properties BOOT-INF/classes/
 
 WORKDIR /app/src/main/resources
 
-
 ADD ./run.sh ./run.sh
 RUN sed -i 's/\r$//' run.sh
 RUN chmod +x 'run.sh'
+
+ADD /target/${PROJECT_NAME}-0.0.1-SNAPSHOT.jar ${PROJECT_NAME}-0.0.1-SNAPSHOT.jar
--- run.sh
+++ run.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
+echo "Start"
 echo ACTIVE_PROFILE: $ACTIVE_PROFILE
 echo API_VERSION: $API_VERSION
 echo JAVA_OPTS: $JAVA_OPTS
 echo PROJECT_NAME: $PROJECT_NAME
+echo "Get Environment Variables"
+
+aws configure set aws_access_key_id AKIA3H6BHXPJ2HYRI3W5
+aws configure set aws_secret_access_key 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
+aws configure set default.region ap-southeast-1
+aws configure set default.output json
+aws secretsmanager get-secret-value --secret-id arn:aws:secretsmanager:ap-southeast-1:772962499539:secret:secret/spartan/db-0fkj3U --query SecretString --output text | jq -r 'to_entries|map("\(.key)=\(.value)")|.[]' > .env
+export $(xargs <.env)
+export SPRING_DATASOURCE_URL=jdbc:postgresql://${SECRET_ENV_DB_MASTER_URL}:${SECRET_ENV_DB_PORT}/${SECRET_ENV_DB_DATABASE_NAME}
+export SPRING_DATASOURCE_USERNAME=${SECRET_ENV_DB_USERNAME}
+export SPRING_DATASOURCE_PASSWORD=${SECRET_ENV_DB_PASSWORD}
+export
+
+java -jar $JAVA_OPTS -Dspring.profiles.active=$ACTIVE_PROFILE /app/${PROJECT_NAME}-0.0.1-SNAPSHOT.jar
