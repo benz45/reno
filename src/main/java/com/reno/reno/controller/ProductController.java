481e1fbfac9b534fc3adc86fe3c2e316c42ec23d
Veerapan Boonbuth <benz45.th@gmail.com>
2024-03-09 04:23:18 +0700

    fix: 401 api get product page

--- src/main/java/com/reno/reno/controller/ProductController.java
+++ src/main/java/com/reno/reno/controller/ProductController.java
@@ -1,14 +1,15 @@
 package com.reno.reno.controller;
 
-import jakarta.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.reno.reno.business.ProductBusiness;
@@ -19,24 +20,29 @@ import com.reno.reno.model.product.CreateProductRequest;
 import com.reno.reno.model.product.CreateProductResponse;
 import com.reno.reno.model.product.ProductPageResponse;
 
+import jakarta.validation.Valid;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
 @Validated
 @RestController
+@RequestMapping("/api/e-commerce-info")
 public class ProductController {
     private @Autowired ProductBusiness productBusiness;
     private @Autowired ProductBusinessFlow productBusinessFlow;
 
-    @PostMapping("/e-commerce-info/product")
+    @PostMapping("/product")
     public CreateProductResponse postProduct(@Valid @RequestBody CreateProductRequest request) throws ApiException {
         return productBusinessFlow.shouldCreateProduct(request);
     }
 
-    @GetMapping("/e-commerce-info/product/{id}")
+    @GetMapping("/product/{id}")
     public CreateProductResponse getProductById(@PathVariable Long id) throws ApiException {
         return productBusiness.shouldGetProductAndProductImageByIdOrElseThrow(id);
     }
 
-    @GetMapping("/e-commerce-info/product")
-    public PageResponse<ProductPageResponse> getProduct(String productName, Pageable pageable) throws ApiException {
+    @GetMapping("/product")
+    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+    public PageResponse<ProductPageResponse> getProductPage(String productName, Pageable pageable) throws ApiException {
         return productBusinessFlow.shouldGetProductPage(productName, pageable);
     }
 }
