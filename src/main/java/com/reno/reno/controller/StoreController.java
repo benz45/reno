9bd72df2d512c9cb7fee14ebe945b122bba33b35
Veerapan Boonbuth <benz45.th@gmail.com>
2024-03-13 19:35:34 +0700

    feature: set PreAuthorize to api store

--- src/main/java/com/reno/reno/controller/StoreController.java
+++ src/main/java/com/reno/reno/controller/StoreController.java
@@ -4,10 +4,13 @@ import jakarta.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.reno.reno.businessflow.StoreBusinessFlow;
@@ -18,21 +21,25 @@ import com.reno.reno.model.store.CreateStoreResponse;
 import com.reno.reno.model.store.StorePageFilter;
 import com.reno.reno.model.store.StorePageResponse;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @Validated
 @RestController
+@RequestMapping("/api/e-commerce-info")
 public class StoreController {
 
     private @Autowired StoreBusinessFlow storeBusinessFlow;
 
-    @GetMapping("/e-commerce-info/store")
+    @GetMapping("/store")
+    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
     public PageResponse<StorePageResponse> getStorePages(String storeName, Integer customerId, Pageable pageable)
             throws ApiException {
         StorePageFilter filter = new StorePageFilter(storeName, customerId, pageable);
         return storeBusinessFlow.getStorePages(filter);
     }
 
-    @PostMapping("/e-commerce-info/store")
-    public CreateStoreResponse postStores(@Valid @RequestBody CreateStoreRequest request) throws ApiException {
+    @PostMapping("/store")
+    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+    public CreateStoreResponse postStore(@Valid @RequestBody CreateStoreRequest request) throws ApiException {
         return storeBusinessFlow.createStore(request);
     }
 }
